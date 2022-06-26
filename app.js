90e245d5549f386b1f9bcc8dcd6d559305a67f78
Paddy Curé <paddy.cure@gmail.com>
2022-06-26 21:08:49 +0100

    add cors

--- app.js
+++ app.js
@@ -2,11 +2,13 @@
 const fs = require("fs")
 const express = require("express")
 const helmet = require("helmet")
+const cors = require("cors")
 
 const app = express()
 const port = 3000
 
 app.use(helmet())
+app.use(cors())
 
 app.use("/tokens", express.static("tokens.json"))
 app.use("/logos", express.static("logos"))
