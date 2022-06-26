497a571c618a16fda368c19ba77583756d0a6adb
Paddy Curé <paddy.cure@gmail.com>
2022-06-26 17:02:30 +0100

    basic security

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 
 const express = require("express")
+const helmet = require("helmet")
+
 const app = express()
 const port = 3000
+
+app.use(helmet())
 
 app.use("/tokens", express.static("tokens.json"))
 app.use("/logos", express.static("logos"))
